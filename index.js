cd46880743b62e1f8a55f0eeee1e1220392e0a60
Gabrielle Bellini <gbellini90@gmail.com>
2018-08-22 03:05:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string){
-  log.console("HELLO")
+  console.log('HELLO') 
 }
 function logWhisper(string){
-  log.console("hello")
+  console.log("hello")
 }
