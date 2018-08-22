2d3e1dfd1237042013d0ead0c45fbf797f46d907
Gabrielle Bellini <gbellini90@gmail.com>
2018-08-22 03:37:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,11 +11,5 @@ function logWhisper(string){
   console.log("string.toLowerCase()")
 }
 function sayHiToGrandma(string)
-if (string === string.toLowerCase)()
-  return"I can\'t hear you!;
-} else if(string.toUpperCase()===string) {
-  return"YES INDEED!;
-} else if(string==="I love you, Grandma") {
-  return "I love you, too!";
-}
-};
+if (string === string.toLowerCase())
+return "I can't hear you!";
